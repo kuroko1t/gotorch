54ba5824ca16198331caf9fef3b1dd2b0286724a
kuroko <kurosawa.yk@gmail.com>
2019-05-30 21:18:58 +0900

    fix libtorch url

--- make.sh
+++ make.sh
@@ -34,7 +34,7 @@ if [ "$FLAG_GPU" ]; then
     fi
 else
     if [ ! -e libtorch ]; then
-        wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+        wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-latest.zip
         unzip libtorch-shared-with-deps-latest.zip
         rm -f libtorch-shared-with-deps-latest.zip
     fi
