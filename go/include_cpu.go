40c7d07df6379fde7aaa51f7deeaa84afa7cfb19
kuroko <kurosawa.yk@gmail.com>
2020-06-04 18:04:02 +0900

    update

---
+++ go/include_cpu.go
@@ -0,0 +1,32 @@
+// +build cpu
+
+/*
+MIT License
+
+Copyright (c) 2019 kurosawa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package torch
+
+// #cgo CXXFLAGS: -g -D_GLIBCXX_USE_CXX14_ABI=0 -I${SRCDIR} -I$GOTORCH_DIR/libtorch/include/ -I$GOTORCH_DIR/libtorch/include/torch/csrc/api/include/
+// #cgo LDFLAGS: -L${SRCDIR}/../libtorch/lib -lc10 -ltorch -lpthread -lcuda -ltorch_cpu -ltorch_cuda
+// #cgo LDFLAGS: -lpthread -lstdc++
+// #cgo CXXFLAGS: -I${SRCDIR}/../libtorch/include/ -I${SRCDIR}/../libtorch/include/torch/csrc/api/include/ -I${SRCDIR}/../cpp
+import "C"
